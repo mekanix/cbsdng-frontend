ec6561fac9b873b37bd70dd0066ddcee39a7739b
Goran Mekić <meka@tilda.center>
2020-11-14 11:03:30 +0100

    Display messages retrieved via websocket

--- src/pages/socket/store.js
+++ src/pages/socket/store.js
@@ -4,5 +4,10 @@ import io from 'socket.io-client'
 export default class SocketStore {
   constructor() {
     this.socket = io({ transports: ['websocket'] })
+    this.socket.on('output', this.onOutput)
+  }
+
+  onOutput = (data) => {
+    console.log(data)
   }
 }
