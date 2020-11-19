78767da3a25a6fc36c72c340f7c939cb2c2882b8
Goran Mekić <meka@tilda.center>
2020-11-19 21:59:12 +0100

    Update

--- src/routing.js
+++ src/routing.js
@@ -24,22 +24,22 @@ const Routing = () => {
   window.rest.API_ROOT = API_ROOT
   return (
     <Switch>
-      <Route exact path="/" component={Landing.detail} />
-      <Route exact path="/confirm/:token" component={Auth.confirm} />
-      <Route exact path="/dashboard" component={Dashboard.detail} />
-      <Route exact path="/instances" component={Instance.list} />
+      <Route exact path="/" component={Landing.Detail} />
+      <Route exact path="/confirm/:token" component={Auth.Confirm} />
+      <Route exact path="/dashboard" component={Dashboard.Detail} />
+      <Route exact path="/instances" component={Instance.List} />
       <Route exact path="/login" component={Auth.Login} />
-      <Route exact path="/profile" component={Profile.detail} />
+      <Route exact path="/profile" component={Profile.Detail} />
       <Route exact path="/register" component={Auth.Register} />
       <Route exact path="/reset" component={Auth.Reset} />
       <Route exact path="/reset/:token" component={Auth.ChangePassword} />
-      <Route exact path="/role/:id" component={Role.detail} />
-      <Route exact path="/roles" component={Role.list} />
-      <Route exact path="/roles/:page" component={Role.list} />
+      <Route exact path="/role/:id" component={Role.Detail} />
+      <Route exact path="/roles" component={Role.List} />
+      <Route exact path="/roles/:page" component={Role.List} />
       <Route exact path="/socket" component={Socket.Interactive} />
-      <Route exact path="/user/:id" component={User.detail} />
-      <Route exact path="/users" component={User.list} />
-      <Route exact path="/users/:page" component={User.list} />
+      <Route exact path="/user/:id" component={User.Detail} />
+      <Route exact path="/users" component={User.List} />
+      <Route exact path="/users/:page" component={User.List} />
       <Route path="*" component={NoPage.Detail} />
     </Switch>
   )
